2f5eb1ad72e37c367eea8a9762226a520e8243df
Logan Guillory <97050508+loganguillory@users.noreply.github.com>
2022-03-24 13:26:06 -0400

    commit tree 53b8a615ee349bb6b3986b43440600f0428ca7a2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+console.log("The DOM has loaded");
+const text = document.getElementById("text").textContent = "This is really cool!";
+console.log(text);
+})
+
+
